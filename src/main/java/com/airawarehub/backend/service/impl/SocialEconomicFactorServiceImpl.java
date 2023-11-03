593dcac612f8cad3e3d7fb54f10694a9353d04ca
rajuswesust <singhoraju8@gmail.com>
2023-11-04 03:30:02 +0600

    minor error fix

--- src/main/java/com/airawarehub/backend/service/impl/SocialEconomicFactorServiceImpl.java
+++ src/main/java/com/airawarehub/backend/service/impl/SocialEconomicFactorServiceImpl.java
@@ -1,7 +1,9 @@
 package com.airawarehub.backend.service.impl;
 
 import com.airawarehub.backend.service.SocialEconomicFactorService;
+import org.springframework.stereotype.Service;
 
+@Service
 public class SocialEconomicFactorServiceImpl implements SocialEconomicFactorService {
     @Override
     public String getTotalGdp(String countryName) {
